353b93a6c09564f052fb1c76652157e58a3e0154
wadams-dev <wadams-dev@gmail.com>
2020-06-27 17:11:26 -0400

    update img tag

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class Card extends React.Component {
     const profile = this.props;
     return (
       <div className="github-profile">
-        <img src={profile.avatar_url} />
+        <img src={profile.avatar_url} alt=""/>
         <div className="info">
           <div className="name">{profile.name}</div>
           <div className="company">{profile.company}</div>
